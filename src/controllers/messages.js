4e055a891f41761f1ef1b92798a15aacb5b8897b
Lero06 <57032316+Lero06@users.noreply.github.com>
2022-11-23 15:35:05 +0100

    CHAT FINAL.

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,5 +1,16 @@
 import {connection} from '../database'
 
+export const getUserMessages = async (req, res) => {
+    const conn = await connection();
+    const [result] = await conn.query('SELECT * FROM messages WHERE userId =?', [req.params.userId]);
+    res.json(result);
+}
+
+export const getChatMessages = async (req, res) => {
+    const conn = await connection();
+    const [result] = await conn.query('SELECT * FROM messages WHERE chatId =?', [req.params.chatId]);
+    res.json(result);
+}
 
 export const getMessages = async (req, res) => {
     const conn = await connection();
@@ -9,10 +20,11 @@ export const getMessages = async (req, res) => {
 
 export const saveMessage = async (req, res) => {
     const conn = await connection();
-    const [result] = await conn.query("INSERT INTO messages(content, userId, mDate) VALUES (?,?,?)", [
+    const [result] = await conn.query("INSERT INTO messages(content, userId, mDate, chatId) VALUES (?,?,?, ?)", [
         req.body.content || "",
         req.body.userId || "",
-        req.body.mDate || ""
+        req.body.mDate || "",
+        req.body.chatId || ""
     ]);
     res.json({
         id: result.insertId,
